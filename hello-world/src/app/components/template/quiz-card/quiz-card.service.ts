705ad776e2af7ce56eca0b3f91a2fa8210b80216
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2021-10-10 03:48:55 -0300

    fix

--- hello-world/src/app/components/template/quiz-card/quiz-card.service.ts
+++ hello-world/src/app/components/template/quiz-card/quiz-card.service.ts
@@ -4,16 +4,16 @@ import { Options, Quiz } from './quiz-card.model';
 @Injectable()
 export class QuizCardService {
   quizzes: Quiz[] = [
-    new Quiz(1, "questão 1?",new Options({text:"luca",isAnswer:false})),
+    new Quiz(1, "questão 1?", new Options({ text: "Alternativa Errada 1", isAnswer: false }, { text: "Alternativa Errada 2", isAnswer: false }, { text: "Alternativa Correta 3", isAnswer: true }, { text: "Alternativa Errada 4", isAnswer: false }, { text: "Alternativa Errada 5", isAnswer: false },)),
     new Quiz(1, "questão 2?"),
     new Quiz(1, "questão 3?"),
     new Quiz(1, "questão 4?"),
     new Quiz(1, "questão 5?")
   ];
 
-  constructor() {}
+  constructor() { }
 
-   listQuizzes():Quiz[]{
-     return this.quizzes;
-   }
+  listQuizzes(): Quiz[] {
+    return this.quizzes;
+  }
 }
